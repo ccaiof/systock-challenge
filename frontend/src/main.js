6e167e716e0790133895266a01e9b7a1e2eb0d07
Caio Santos <caio.francelinosena@gmail.com>
2026-03-18 20:08:23 -0400

    feat: add vuetify and axios integration

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,12 +1,21 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
+import '@fontsource/roboto/100.css'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import '@fontsource/roboto/900.css'
+
 import App from './App.vue'
 import router from './router'
+import { vuetify } from './plugins/vuetify'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
